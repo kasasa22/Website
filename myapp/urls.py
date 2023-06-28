7e5e61b02f4325541c3c70009f09c38ebb0d1b3b
Kasasa Trevor <kasasatrevor25@gmail.com>
2023-06-28 01:44:44 -0700

    new changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('counter', views.counter, name='counter')
+    path('admin', views.admin_index, name='admin_index')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-   
+def index(request):   
     return render(request, 'index.html')
+def admin_index(request):
+    return render(request, 'admin-index.html')
 
-def counter(request):
-    text = request.POST ['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html',{'amount':amount_of_words})
