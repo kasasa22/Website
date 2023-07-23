8d0c5fce8854d8a979bc505918bd2112b294edaf
Kasasa Trevor <kasasatrevor25@gmail.com>
2023-07-23 04:29:20 -0700

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.welcome, name='welcome'),
     path('admin', views.admin_index, name='admin_index'),
     path('category', views.category, name='category'),
     path('supplier', views.supplier, name='supplier'),
@@ -9,6 +9,14 @@ urlpatterns = [
     path('medicine', views.medicine, name='medicine'),
     path('sales-detail', views.sales_detail, name='sales_detail'),
     path('report', views.report, name='report'),
-    path('company', views.company, name='company')
+    path('company', views.company, name='company'),
+    path('add-category', views.add_category, name='add-category'),
+    path('add-supplier', views.add_supplier, name='add-supplier'),
+    path('add-cashier', views.add_cashier, name='add-cashier'),
+    path('add-medicine', views.add_medicine, name='add-medicine'),
+    path('cashier-index', views.cashier_index, name='cashier_index'),
+    path('sales', views.sales, name='sales'),
+    path('daily-sales', views.daily_sales, name='daily_sales'),
+    path('profile', views.sales, name='profile')
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):   
-    return render(request, 'index.html')
+def welcome(request):   
+    return render(request, 'welcome.html')
 def admin_index(request):
     return render(request, 'admin-index.html')
 def category(request):
@@ -21,3 +21,19 @@ def report(request):
 def company(request):
     return render(request, 'company.html')
 
+def add_category(request):
+    return render(request, 'add-category.html',)
+def add_supplier(request):
+    return render(request, 'add-supplier.html',)
+def add_cashier(request):
+    return render(request, 'add-cashier.html',)
+def add_medicine(request):
+    return render(request, 'add-medicine.html',)
+def cashier_index(request):
+    return render(request, 'cashier-index.html')
+def sales(request):
+    return render(request, 'sales.html')
+def daily_sales(request):
+    return render(request, 'daily-sales.html')
+def profile(request):
+    return render(request, 'profile.html')
